ce0a24a98d5657dda2f28c798d3c62e5c2c49e4d
Nathan Puls <71989998+nathanpuls@users.noreply.github.com>
2024-02-13 07:59:46 -0600

    Create x.js

---
+++ x.js
@@ -0,0 +1,8 @@
+// Script to handle input and print to console
+document.getElementById('submitBtn').addEventListener('click', function() {
+    // Get the input value
+    var inputValue = document.getElementById('consolewriter').value;
+
+    // Print the input value to the console
+    console.log(inputValue);
+});
